3606372b74b36964672a78d125cc55aba3e448c7
= <alessandrotornesi@iisvittorioveneto.it>
2021-03-26 15:31:11 +0100

    Login form almost completed

--- src/it/iisvittorioveneto/quartaB/gruppo3/Main.java
+++ src/it/iisvittorioveneto/quartaB/gruppo3/Main.java
@@ -1,23 +1,21 @@
 package it.iisvittorioveneto.quartaB.gruppo3;
 
 import it.iisvittorioveneto.quartaB.gruppo3.gui.Login;
-import it.iisvittorioveneto.quartaB.gruppo3.mariadb.JDBC;
 
 import javax.swing.*;
 import java.awt.*;
-import java.sql.SQLException;
 
 public class Main {
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Login");
+        JFrame jFrame = new JFrame("Login");
         Login loginPage = new Login();
-        frame.setContentPane(loginPage.getContentPane());
-        frame.setSize(500, 500);
+        jFrame.setContentPane(loginPage.getContentPane());
+        jFrame.setSize(500, 250);
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        jFrame.setLocation(dim.width/2-jFrame.getSize().width/2, dim.height/2-jFrame.getSize().height/2);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setVisible(true);
     }
 
 }
--- src/it/iisvittorioveneto/quartaB/gruppo3/gui/Login.java
+++ src/it/iisvittorioveneto/quartaB/gruppo3/gui/Login.java
@@ -4,6 +4,7 @@ import it.iisvittorioveneto.quartaB.gruppo3.mariadb.JDBC;
 
 import javax.swing.*;
 import java.sql.SQLException;
+import java.util.regex.Pattern;
 
 public class Login {
     private JPanel contentPane;
@@ -11,16 +12,34 @@ public class Login {
     private JPasswordField passwordField;
     private JButton logInButton;
     private JButton signInButton;
+    private JLabel noUserFoundLabel;
+    private JLabel invalidEmailLabel;
 
     public Login() {
         logInButton.addActionListener(e -> {
             try {
-                String username = JDBC.logIn(emailTextField.getText(), passwordField.getPassword());
-                if (username != null);
+                Pattern pattern = Pattern.compile("^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$");
+                if (pattern.matcher(emailTextField.getText()).matches()) {
+                    String username = JDBC.logIn(emailTextField.getText(), passwordField.getPassword());
+                    if (username != null);
                     //TODO go to home page
+                    else{
+                        invalidEmailLabel.setText("");
+                        noUserFoundLabel.setText("There is no user with those credentials");
+                    }
+
+                }
+                else {
+                    noUserFoundLabel.setText("");
+                    invalidEmailLabel.setText("This email has an incorrect format");
+                }
+
             } catch (SQLException ex) {
                 ex.printStackTrace();
             }
+        });
+        signInButton.addActionListener(e -> {
+            //TODO go to sign in form
         });
     }
 
