efe720321f927b2d71fefeaa4ff1518d86b37758
andrecasa03 <casagrandeandrea6@gmail.com>
2021-03-26 16:10:11 +0100

    signIn for Implemented

---
+++ src/it/iisvittorioveneto/quartaB/gruppo3/gui/SignIn.java
@@ -0,0 +1,15 @@
+package it.iisvittorioveneto.quartaB.gruppo3.gui;
+
+import javax.swing.*;
+
+public class SignIn {
+    private JTextField name;
+    private JTextField lastName;
+    private JTextField email;
+    private JLabel LabelName;
+    private JLabel LabelEmail;
+    private JLabel LabelLName;
+    private JPasswordField password;
+    private JButton LoginRetButton;
+    private JButton SignInButton;
+}
