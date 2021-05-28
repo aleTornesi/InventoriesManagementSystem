21ef6a310c364d2ddb13db1bd7d9c921d83de906
Davide0611 <davidebottega06@gmail.com>
2021-05-28 08:46:03 +0200

    Class test implemented

---
+++ src/it/iisvittorioveneto/quartaB/gruppo3/inventoriesmanagementsystem/InventoryTest.java
@@ -0,0 +1,36 @@
+package it.iisvittorioveneto.quartaB.gruppo3.inventoriesmanagementsystem;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryTest {
+
+    @BeforeAll
+    static void setUpClass(){
+        System.out.println("Inizio");
+    }
+
+    @AfterAll
+    static void tearDownClass(){
+        System.out.println("Fine");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("Inizio test");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("Fine test");
+    }
+
+    @Test
+    void addProduct() {
+        Inventory inventory = new Inventory();
+        Product product = new Product();
+        inventory.addProduct(product);
+        assertTrue(inventory.getProducts()[0]==product);
+    }
+}
---
+++ src/it/iisvittorioveneto/quartaB/gruppo3/inventoriesmanagementsystem/TagTest.java
@@ -0,0 +1,45 @@
+package it.iisvittorioveneto.quartaB.gruppo3.inventoriesmanagementsystem;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TagTest {
+
+    @BeforeAll
+    static void setUpClass(){
+        System.out.println("Inizio");
+    }
+
+    @AfterAll
+    static void tearDownClass(){
+        System.out.println("Fine");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("Inizio test");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("Fine test");
+    }
+
+    @Test
+    public void addProduct() {
+        Tag tag = new Tag();
+        Product product = new Product();
+        tag.addProduct(product);
+        assertTrue(tag.getProducts()[0]==product);
+    }
+
+    @Test
+    public void removeProduct() {
+        Tag tag = new Tag();
+        Product product = new Product();
+        tag.addProduct(product);
+        tag.removeProduct(product);
+        assertTrue(tag.getProducts().length==0);
+    }
+}
